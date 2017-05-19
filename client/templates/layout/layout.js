ce35d6f2ec3b0dbcaa3a08c0d3f7f9fc4e521c67
macbook <macbook@macbooks-MacBook-Pro.local>
2017-05-20 01:47:42 +0600

    Initial commit

---
+++ client/templates/layout/layout.js
@@ -0,0 +1,18 @@
+import '../../../imports/api/place.js';
+
+Template.registerHelper('formateDate', (date)=>{
+	return moment(date).format('MMMM YYYY');
+});
+
+Meteor.startup(() => {
+    // code to run on server at startup
+    smoothScroll.init({
+      selector: '[data-scroll]', // Selector for links (must be a valid CSS selector)
+      selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+      speed: 500, // Integer. How fast to complete the scroll in milliseconds
+      easing: 'easeInOutCubic', // Easing pattern to use
+      updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+      offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+      callback: function ( toggle, anchor ) {} // Function to run after scrolling
+    });
+});
